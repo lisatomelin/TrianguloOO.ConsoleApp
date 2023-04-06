85abbdf7b1978d00fe581908a2594ad1dcc9ca28
LisaTomelin <lisa.tomelin@gmail.com>
2023-04-06 14:09:16 -0300

    Add project files.

---
+++ TrianguloOO.ConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace TrianguloOO.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Triangulo lados = new Triangulo();
+
+            
+            Console.WriteLine("Informe o lado 1: ");
+            lados.lado1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Informe o lado 2: ");
+            lados.lado2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Informe o lado 3: ");
+            lados.lado3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("É possivel formar um triângulo? " + lados.FormarTriangulo());
+            Console.WriteLine("É um triângulo Escaleno? " + lados.FormarEscaleno());
+            Console.WriteLine("É um triângulo Isóceles? " + lados.FormarIsoceles());
+            Console.WriteLine("É um triângulo Equilátero? " + lados.FormarEquilatero());
+
+            Console.WriteLine();
+            
+
+        }
+    }
+}
---
+++ TrianguloOO.ConsoleApp/Triangulo.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrianguloOO.ConsoleApp
+{
+    internal class Triangulo
+    {
+        public int lado1;
+        public int lado2;
+        public int lado3;
+
+        public bool FormarTriangulo()
+        {
+            if (lado1 < lado2 + lado3 && lado2 < lado1 + lado3 && lado3 < lado1 + lado2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        
+        public bool FormarEscaleno() 
+        {
+            if (lado1 != lado2 && lado2 != lado3 && lado1 != lado3)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool FormarIsoceles()
+        {
+            if (lado1 == lado2  || (lado1 ==lado3) || lado2 == lado3)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool FormarEquilatero() 
+        {
+            if (lado1 == lado2 && lado2 == lado3)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+    }
+
+
+}
